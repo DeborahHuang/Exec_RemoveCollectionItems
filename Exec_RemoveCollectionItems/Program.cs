f49773a0d90ade877afefb64ee41fef96b52ade0
88693 <88693@LAPTOP-FS9ST1LD>
2022-10-24 23:13:30 +0800

    加入專案檔案。

---
+++ Exec_RemoveCollectionItems/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exec_RemoveCollectionItems
+{//在集合中移除多個項目
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			List<int> num = new List<int>();
+			num.Add(1);
+			num.Add(2);
+			num.Add(3);
+			num.Add(4);
+			num.Add(5);
+			num.Remove(2);
+			num.Remove(3);
+			num.Remove(4);
+
+			foreach (int item in num)
+			{
+				Console.WriteLine(item);
+			}
+		}
+	}
+}
